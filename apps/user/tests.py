1f3d62d1dd186e5f9498cf121b09874cf42590e8
pouya.fallahi <pouya.fallahi80@gmail.com>
2024-07-22 19:53:10 +0330

    NOTHING

--- apps/user/tests.py
+++ apps/user/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import User
 
-# Create your tests here.
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        User.objects.create('user1', 'password1')
+        User.objects.create('user2', 'password2')
+        User.objects.create('user3', 'password3')
+        User.objects.create('user4', 'password4')
+        User.objects.create('user5', 'password5')
