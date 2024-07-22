9da76f88aabf637dcdca5cd01ac1ea038fc31f98
pouya.fallahi <pouya.fallahi80@gmail.com>
2024-07-22 19:56:11 +0330

    FIX | Finding the right model for the recommended model of the books, but there is a small bug to display in endpiont

--- apps/books/book_suggestion.py
+++ apps/books/book_suggestion.py
@@ -1,38 +1,42 @@
-import numpy as np
 import pandas as pd
-from .models import Rating, Book
-from sklearn.decomposition import TruncatedSVD
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+from .models import Book, Rating
 
 
-class Recommender:
-    def __init__(self, n_components=5):
-        self.n_components = n_components
-        self.model = None
-        self.ratings_matrix = None
-        self.user_id_to_index = {}
-        self.book_id_to_index ={}
+class ContentBasedRecommender:
+    def __init__(self):
+        self.books = None
+        self.tfidf_matrix = None
+        self.indices = None
 
     def fit(self):
-        ratings = Rating.objects.all().values('user_id', 'book_id', 'score')
-        df = pd.DataFrame(ratings)
-        self.user_id_to_index = {user_id: index for index, user_id in enumerate(df['user_id'].unique())}
-        self.book_id_to_index = {book_id: index for index, book_id in enumerate(df['book_id'].unique())}
-        ratings_matrix = pd.pivot_table(df, index='user_id', columns='book_id', values='score', fill_value=0)
-        self.ratings_matrix = ratings_matrix.values
-        self.model = TruncatedSVD(n_components=self.n_components)
-        self.ratings_matrix_reduced = self.model.fit_transform(self.ratings_matrix)
+        books = Book.objects.all().values('id', 'title', 'author', 'genre')
+        self.books = pd.DataFrame(books)
+
+        self.books['features'] = self.books['title'] + ' ' + self.books['author'] + ' ' + self.books['genre']
+
+        tfidf_vectorizer = TfidfVectorizer(stop_words='english')
+        self.tfidf_matrix = tfidf_vectorizer.fit_transform(self.books['features'])
+        self.indices = pd.Series(self.books.index, index=self.books['id'])
 
     def recommend(self, user_id):
-        if self.model is None:
-            raise RuntimeError('Model must be fit before recommend.')
+        user_ratings = Rating.objects.filter(user_id=user_id).values('book_id', 'score')
+        user_ratings_df = pd.DataFrame(user_ratings)
 
-        if user_id not in self.user_id_to_index:
-            raise ValueError('User not found.')
+        recommended_books = pd.DataFrame()
+        for book_id in user_ratings_df['book_id']:
+            if book_id in self.indices.index:
+                idx = self.indices[book_id]
+                sim_scores = list(enumerate(linear_kernel(self.tfidf_matrix[idx], self.tfidf_matrix).flatten()))
+                sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+                sim_scores = sim_scores[1:11]
 
-        user_index = self.user_id_to_index[user_id]
-        user_ratings = np.dot(self.ratings_matrix_reduced[user_index, :], self.model.components_)
+                book_indices = [i[0] for i in sim_scores]
+                similar_books = self.books.iloc[book_indices]
+                recommended_books = pd.concat([recommended_books, similar_books])
 
-        recommended_book_indices = np.argsort(user_ratings)[::-1]
-        recommended_books = [list(self.book_id_to_index.keys())[index] for index in recommended_book_indices]
-
-        return recommended_books
+        recommended_books = recommended_books.drop_duplicates()
+        recommended_books = recommended_books[~recommended_books['id'].isin(user_ratings_df['book_id'])]
+        print(recommended_books.reset_index(drop=True))
+        return dict(recommended_books.reset_index(drop=True))
