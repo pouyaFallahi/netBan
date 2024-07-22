a1a38353514e2a8ff2ab638ecc48919d7eb66a45
pouya.fallahi <pouya.fallahi80@gmail.com>
2024-07-22 16:08:08 +0330

    UPDATE |Faxing the problem of changing points and adding functions for book suggestion, but it still has bugs.

--- apps/books/views.py
+++ apps/books/views.py
@@ -1,12 +1,17 @@
 from .models import Book, Rating
-from django.shortcuts import render
-from rest_framework import viewsets, generics
+from .filters import BookFilter
+from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.views import APIView
+from .book_suggestion import Recommender
+from rest_framework.response import Response
+from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import BookSerializer, RatingSerializer, BookFilterSerializer
-from django_filters import rest_framework as filters
 
 
-class BookListView(generics.ListAPIView):
+class BookListView(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = []
@@ -18,26 +23,42 @@ class RatingViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        book_id = self.request.data.get('book')
+        user = self.request.user
+        score = self.request.data.get('score')
+        rating, created = Rating.objects.get_or_create(
+            user=user,
+            book_id=book_id
+        )
+
+        if not created:
+            serializer.instance = rating
+            serializer.save()
+        else:
+            serializer.save(user=user, book_id=book_id, rating=rating)
 
     def get_queryset(self):
         return Rating.objects.filter(user=self.request.user)
 
 
-class BookFilter(filters.FilterSet):
-    genre = filters.CharFilter(lookup_expr='icontains')
+class BookFilterList(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = BookFilter
+    permission_classes = [IsAuthenticated]
 
-    class Meta:
-        model = Book
-        fields = ['genre']
 
+class RecommendBookView(APIView):
+    permission_classes = [IsAuthenticated]
+    def __init__(self, **kwargs):
+        self.recommender = Recommender()
+        self.recommender.fit()
+        super().__init__(**kwargs)
 
-class BookFilterList(generics.ListAPIView):
-    serializer_class = BookFilterSerializer
-    authentication_classes = []
-    filter_backends = [filters.DjangoFilterBackend]
-
-    def get_queryset(self):
-        genre = self.kwargs['genre']
-        return Book.objects.filter(genre=genre)
-
+    def get(self, request, *args, **kwargs):
+        user_id = request.user.id
+        recommended_books_ids = self.recommender.recommend(user_id)
+        recommended_books = Book.objects.filter(id__in=recommended_books_ids)
+        serializer = BookSerializer(recommended_books, many=True)
+        return Response(serializer.data)
