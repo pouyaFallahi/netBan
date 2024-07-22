4160c0551cc715b25ad1a8adb05ea3baa4363fbe
pouya.fallahi <pouya.fallahi80@gmail.com>
2024-07-22 16:06:28 +0330

    ADD | Added address to suggest book and cleaning

--- apps/books/urls.py
+++ apps/books/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path, re_path
-from apps.books.views import BookListView, BookFilterList
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from apps.books.views import BookListView, BookFilterList, RecommendBookView, RatingViewSet
 
 app_name = 'books'
 
+router = DefaultRouter()
+router.register(r'filter', BookFilterList, basename='books')
+router.register(r'ratings', RatingViewSet, basename='ratings')
+
 urlpatterns = [
-    path('all', BookListView.as_view(), name='book_list'),
-    re_path('filter/', BookFilterList.as_view()),
+    path('all/', BookListView.as_view(), name='book_list'),
+    path('', include(router.urls)),
+    path('recommend/', RecommendBookView.as_view(), name='recommend_book'),
 ]
