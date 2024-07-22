f0aad420939e67fff4e7c282f7ee0c9a1a9afbf2
pouya.fallahi <pouya.fallahi80@gmail.com>
2024-07-22 16:12:00 +0330

    UPDATE | A series of URLs were removed and moved to the correct place

--- config/urls.py
+++ config/urls.py
@@ -1,17 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from apps.books.views import BookListView, RatingViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
-router = routers.DefaultRouter()
-router.register(r'ratings', RatingViewSet, basename='ratings')
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', include('apps.books.urls')),
-    path('api/', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
