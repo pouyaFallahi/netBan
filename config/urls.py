2fafc7fe7cc4cfb864c289764ca05169be63b6fd
pouya.fallahi <pouya.fallahi80@gmail.com>
2024-07-22 22:29:20 +0330

    ADD | user.urls.py add to config.urls.py

--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', include('apps.books.urls')),
+    path('users/', include('apps.users.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
